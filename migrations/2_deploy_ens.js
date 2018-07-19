e55753e8b869fcffa3fefd78c57b9173b687a269
Premysl Donat <pdonat@seznam.cz>
2018-07-19 09:58:57 +0200

    wip

--- migrations/2_deploy_ens.js
+++ migrations/2_deploy_ens.js
@@ -1,8 +1,5 @@
 const ENS = artifacts.require("ENSRegistry");
-const PublicResolver = artifacts.require("./PublicResolver.sol");
 
 module.exports = async function(deployer) {
   await deployer.deploy(ENS);
-  // see comment in new migration
-  // await deployer.deploy(PublicResolver, ENS.address);
 };
