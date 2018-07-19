df7495244d2554d6521d58b51f1817911812b87f
Ondřej Svoboda <ondrej.svoboda@topmonks.com>
2018-07-19 10:05:00 +0200

    :bug: fix migrations

--- migrations/1531748456_deploy_ens.js
+++ migrations/1531748456_deploy_ens.js
@@ -1,11 +1,11 @@
 const namehash = require("eth-ens-namehash").hash;
 
-const ENS = artifacts.require("test/fixtures/ENSRegistry");
-const PublicResolver = artifacts.require("test/fixtures/PublicResolver");
+const ENS = artifacts.require("ENSRegistry");
+const PublicResolver = artifacts.require("PublicResolver");
 
 module.exports = function(deployer) {
   const rootNode = namehash("eth");
   deployer.deploy(ENS).then(() => {
-    deployer.deploy(PublicResolver, ENS.address);
+    return deployer.deploy(PublicResolver, ENS.address);
   });
 };
--- migrations/1531748979_deploy_tm_registrar.js
+++ migrations/1531748979_deploy_tm_registrar.js
@@ -1,9 +1,10 @@
 const namehash = require("eth-ens-namehash").hash;
 
 const ENS = artifacts.require("test/fixtures/ENSRegistry");
+const PublicResolver = artifacts.require("test/fixtures/PublicResolver");
 const TMRegistrar = artifacts.require("TopmonksRegistrar.sol");
 
-module.exports = function(deployer) {
-  const rootNode = namehash("topmonks.eth");
-  deployer.deploy(TMRegistrar, ENS.address, rootNode);
+module.exports = function(deployer, network, accounts) {
+  const tmNode = namehash("topmonks.eth");
+  deployer.deploy(TMRegistrar, tmNode, ENS.address, PublicResolver.address);
 };
