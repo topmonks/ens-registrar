03a7055ba51c1305b08c0a31abcf7a88d505aae0
Premysl Donat <pdonat@seznam.cz>
2018-07-30 12:34:50 +0200

    wip

--- test/TopmonksRegistrar.test.js
+++ test/TopmonksRegistrar.test.js
@@ -21,14 +21,14 @@ contract('TopmonksRegistrar', async (accounts) => {
       await ens.setSubnodeOwner('0x0', web3.sha3("eth"), accounts[1]);
       await ens.setSubnodeOwner(rootNode, web3.sha3("topmonks"), subject.address, { from: contractOwner });
 
-      var addrDebug = subject.addrDebug();
-      addrDebug.watch((err, result) => {
-        console.log(result.args.msg, result.args.addr);
-      });
-      var addrDebug2 = resolver.addrDebug();
-      addrDebug2.watch((err, result) => {
-        console.log(result.args.msg, result.args.addr);
-      });
+      // var addrDebug = subject.addrDebug();
+      // addrDebug.watch((err, result) => {
+      //   console.log(result.args.msg, result.args.addr);
+      // });
+      // var addrDebug2 = resolver.addrDebug();
+      // addrDebug2.watch((err, result) => {
+      //   console.log(result.args.msg, result.args.addr);
+      // });
 
       console.log('accounts[3] je:', accounts[3]);
       await subject.register(web3.sha3("test"), accounts[3], { from: accounts[3] });
