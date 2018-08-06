59d415535ff1fd472f88402f878d7326866b8cb3
Premysl Donat <pdonat@seznam.cz>
2018-08-06 08:31:21 +0200

    wip

--- c7.js
+++ c7.js
@@ -2,28 +2,28 @@ const namehash = require("eth-ens-namehash").hash;
 
 const Web3 = require('web3');
 const web3 = new Web3('http://127.0.0.1:8545');
-// const web3 = new Web3('ws://127.0.0.1:8545');
 
 const TMRegistrar = require("./build/contracts/TopmonksRegistrar.json");
+const ENSRegistry = require('./build/contracts/ENSRegistry.json');
 
 main = async () => {
-
   const accounts = await web3.eth.getAccounts();
-  const rootNode = namehash("eth");
-  const tmNode = namehash("topmonks.eth");
-  const contractOwner = accounts[1];
-  const address = "0xEf06739814f19c90EB97bc0A13D52a06Af0966d0";
+  console.log("Ucty jsou");
+  console.log(accounts);
 
+  // const ensAddress = "0x1e176145b27DE7d61C75dD32B755DDAfb6c7F93A";
+  // ens = new web3.eth.Contract(ENSRegistry.abi, ensAddress);
+  // var res = await ens.methods.owner(namehash("monika.topmonks.eth"))
+  //   .call();
+  // console.log(res);
+
+  const address = "0x3f3B27Aa272B28096284B728C8832dA292f8f1Fa";
   registrar = new web3.eth.Contract(TMRegistrar.abi, address);
-
-  var res = await registrar.methods.register(web3.utils.sha3("kozicky"), accounts[4])
+  var res = await registrar.methods.register(web3.utils.sha3("kozicky"), namehash("kozicky.topmonks.eth"), accounts[4])
     .send({ from: accounts[4] });
   console.log(res);
 
-  var result = await registrar.methods.neco()
-    .call({ from: accounts[3] });
-  console.log('result is ', result);
+  console.log('po kozicky!');
 }
 
 main();
-console.log('goob!');
--- contracts/TopmonksRegistrar.sol
+++ contracts/TopmonksRegistrar.sol
@@ -1,53 +1,23 @@
 pragma solidity ^0.4.22;
 
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "./ENSRegistry.sol";
 import "./PublicResolver.sol";
 
-contract TopmonksRegistrar is Ownable {
+contract TopmonksRegistrar {
     bytes32 public rootNode;
     ENSRegistry public ens;
     PublicResolver public resolver;
-    address public resolverAddr;
-
-    modifier onlyDomainOwner(bytes32 subnode) {
-        address currentOwner = ens.owner(keccak256(abi.encodePacked(rootNode, subnode)));
-        require(currentOwner == 0 || currentOwner == msg.sender, "Only owner");
-        _;
-    }
 
     constructor(bytes32 _node, address _ensAddr, address _resolverAddr) public {
         rootNode = _node;
         ens = ENSRegistry(_ensAddr);
-        resolverAddr = _resolverAddr;
         resolver = PublicResolver(_resolverAddr);
     }
 
-    function setRootNode(bytes32 _node) public onlyOwner {
-        rootNode = _node;
-    }
-
-    function setResolver(address _resolverAddr) public onlyOwner {
-        resolver = PublicResolver(_resolverAddr);
-    }
-
-    function setNodeOwner(address _newOwner) public onlyOwner {
-        ens.setOwner(rootNode, _newOwner);
-    }
-
-    function setSubnodeOwner(bytes32 _subnode, address _addr) public onlyOwner {
-        ens.setSubnodeOwner(rootNode, _subnode, _addr);
-    }
-
-    function register(bytes32 _subnode, address _addr) public onlyDomainOwner(_subnode) {
+    function register(bytes32 _subnode, bytes32 _node, address _addr) public {
         ens.setSubnodeOwner(rootNode, _subnode, this);
-        bytes32 node = keccak256(abi.encodePacked(rootNode, _subnode));
-        ens.setResolver(node, resolverAddr);
-        resolver.setAddr(node, _addr);
+        ens.setResolver(_node, resolver.address);
+        // resolver.setAddr(_node, _addr);
         ens.setSubnodeOwner(rootNode, _subnode, _addr);
-    }
-
-    function neco() public view returns (string) {
-      return 'ahoj';
     }
 }
