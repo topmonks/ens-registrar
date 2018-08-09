054dac1b1299f3b6638d50a72d4fd5887330ae8b
Premysl Donat <pdonat@seznam.cz>
2018-08-09 12:46:39 +0200

    Update configs

--- truffle.js
+++ truffle.js
@@ -1,6 +1,11 @@
 module.exports = {
   networks: {
-    development: {
+    ganache: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    ganache_cli: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
