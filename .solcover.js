7f0f625f48327c518e7f7839084ce878ee74d4ea
David Votrubec <david.votrubec@gmail.com>
2019-01-02 15:32:12 +0100

    Configuring Solidity Coverage to run locally installed truffle

--- .solcover.js
+++ .solcover.js
@@ -1,4 +1,7 @@
 module.exports = {
   copyPackages: ['openzeppelin-solidity'],
   skipFiles: ['Migrations.sol', 'TestArtifacts.sol'],
+
+  compileCommand: '../node_modules/.bin/truffle compile',
+  testCommand: '../node_modules/.bin/truffle test --network coverage',
 };
