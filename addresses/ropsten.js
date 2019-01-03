85c91d64a083dd31bf8887da082d7bab83e90c71
David Votrubec <david.votrubec@gmail.com>
2019-01-03 11:04:16 +0100

    Upgraded web3 packages. Re-generated json versions of contracts. Still failing with Out of gas on Ropsten

--- addresses/ropsten.js
+++ addresses/ropsten.js
@@ -1,6 +1,12 @@
 module.exports = {
-    // ensAddress: '0x5431a10083b88c346A5F390332A31Ad81C119541', // topmonks 
-    ensAddress: '0x112234455c3a32fd11230c42e7bccd4a84e02010', // official
-    resolverAddress: '0xf6ffc35561f09e5480ea0aabd77e9590d61dd3ce', // topmonks 
-    registrarAddress: '0x1ba0599bbb54e8982dff0dae19cf4571d7673c2d' // topmonks 
+    // '0x5431a10083b88c346A5F390332A31Ad81C119541', // topmonks 
+    // ensAddress: '0x112234455c3a32fd11230c42e7bccd4a84e02010', // OFFICIAL
+    // resolverAddress: '0xf6ffc35561f09e5480ea0aabd77e9590d61dd3ce', // topmonks 
+    // registrarAddress: '0x1ba0599bbb54e8982dff0dae19cf4571d7673c2d' // topmonks 
+
+    // Nove vytvorene, stejne to porad pada na "out of gas".
+    ensAddress: '0x72890E0430096d307Fb6F731b2EC84E0326960Dc', // topmonks, zase neco zlobi, tak jsem ho vytvoril znovu
+    resolverAddress: '0x552b63993085B40AA3FFA31e600Cee40eE0B0a45', // topmonks 
+    registrarAddress: '0x1FBF2D7FD4Eb9c385c7A61de0B010FBa1Cc4e850' // topmonks 
+
 };
--- truffle.js
+++ truffle.js
@@ -15,8 +15,8 @@ module.exports = {
   solc: {
     // Lumir recommends trying to disable optimizer when we get the "out of gas" error
     optimizer: {
-      enabled: true,
-      runs: 200 // indicates how many times the contract is supposed to run.
+      // enabled: true,
+      // runs: 200 // indicates how many times the contract is supposed to run.
       /**
        * From https://solidity.readthedocs.io/en/develop/using-the-compiler.html#compiler-input-and-output-json-description
        * Before you deploy your contract, activate the optimizer when compiling using solc --optimize --bin sourceFile.sol.
@@ -24,7 +24,7 @@ module.exports = {
        * If you want the initial contract deployment to be cheaper and the later function executions to be more expensive, set it to --runs=1. 
        * If you expect many transactions and do not care for higher deployment cost and output size, set --runs to a high number.
        */
-      // enabled: false
+      enabled: false
     }
   },
 
